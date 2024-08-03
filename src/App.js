69034437fcbe742f2f0ec824ea927dac6e1c460f
Sakshi <sakshichoudhary000@gmail.com>
2024-08-03 18:22:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; //check this in hashrouter
+import HomePage from './pages/HomePage';
+import DetailPage from './pages/DetailPage';
+
+
+function App() {
+  return (
+    <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/post/:id" element={<DetailPage />} />
+            </Routes>
+        </Router>
+
+  );
+}
+
+export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import {thunk} from 'redux-thunk';
+import postsReducer from './reducers';
+
+const rootReducer = combineReducers({
+  posts: postsReducer,
+});
+
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+export default store;
